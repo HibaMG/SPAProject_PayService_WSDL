cc8687498b540e5caf2cb283b500aa34879d2b43
hiba ghannam <hiba.mg.g@gmail.com>
2018-05-23 00:51:10 +0300

    code

---
+++ General_WCF_Service_For_Testing/General_WCF_Service_For_Testing/Service1.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace General_WCF_Service_For_Testing
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        /// <summary>
+        /// it's just a service for testing purposes,
+        /// if you pass 1 the return value will be  true
+        /// if you pass anything else, you will receive false
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool GetStatus(int value)
+        {
+            if (value == 1)
+            { return true; }
+
+            else return false;
+        }
+
+      
+    }
+}
